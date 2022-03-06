efc90483545a695304374864371a50e46d8550a6
aliyachen <31674293+aliyachen@users.noreply.github.com>
2022-03-06 12:06:47 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -23,8 +23,48 @@ $(document).ready(function() {
 
 });
 
-$(function() {
+$(document).ready(function() {
 $("#dropdownitems").click(function(){
 	$(".dropitems").slideToggle();
 });
 });
+//------------------------------------MODAL SCRIPTS------------------------------------
+//detect click outside of modal; if so, close modal
+$(document).click(function(e){
+    var clickElement = e.target;  // get the dom element clicked.
+    var elementClassName = e.target.className;  // get the classname of the element clicked
+    myModal = document.getElementById("myModal");
+    opacity = window.getComputedStyle(myModal).getPropertyValue("opacity"); //check visibility of modal
+
+    //if clicked on modal or image (not next/prev buttons) and the modal is visible, close modal
+    if (elementClassName == "slide" && opacity==1) {
+      closeModal();
+    }
+    else if (elementClassName == "modal" && opacity==1) {
+      closeModal();
+    };
+    //alert(elementClassName); check class name
+});
+//arrow keys simulate next and prev
+  document.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 37:
+            //left
+            plusSlides(-1);
+            break;
+        case 38:
+            //up
+            break;
+        case 39:
+            //right
+            plusSlides(1);
+            break;
+        case 40:
+            //down
+            break;
+        case 27:
+            //esc
+            closeModal();
+            break;
+    }
+  };
