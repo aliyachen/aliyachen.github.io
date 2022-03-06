25b5941c728720451ba15903cd74bbc777bfb077
aliyachen <31674293+aliyachen@users.noreply.github.com>
2022-03-06 11:40:43 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	var aboutTopPosition = jQuery('#about').offset().top;
-	
+
 	// When #scroll is clicked
 	jQuery('#scroll').click(function(){
 		// Scroll down to 'catTopPosition'
@@ -8,4 +8,23 @@ $(document).ready(function(){
 		// Stop the link from acting like a normal anchor link
 		return false;
 	});
+});
+
+
+$(document).ready(function() {
+
+    $('.center-con').on('webkitAnimationEnd', function(e) {
+    $(this).addClass('visible');
+  });
+
+    $('.bgimage').on('webkitAnimationEnd', function(e) {
+    $(this).addClass('visible');
+  });
+
+});
+
+$(function() {
+$("#dropdownitems").click(function(){
+	$(".dropitems").slideToggle();
+});
 });
